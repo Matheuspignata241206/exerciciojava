ea39498720c5f911991d005827c7a2eaa596b55d
matheus <matheus.schslarski.santos@escola.pr.gov.br>
2024-09-30 11:26:30 -0300

    iniciando teste de arquivos de java

---
+++ src/Nome.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Nome {
+    private static String nextline;
+
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        String nome = leitura.nextLine();
+
+    }
+}
